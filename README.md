8c9028289a941032e8bb4b435290060a20867d65
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2021-12-05 14:06:18 +0000

    setup

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # KASM
- A high-level assembly-like language.
+
+A high-level assembly-like language.
+
+## How do you use this language?
+
+Read the wiki on the [GitHub](https://github.com/telekiwi/kasm/wiki) page for KASM. It has lots of cool and epic documentation.
+
+## Why does this exist?
+
+KASM exists because I found creating an assembly language in a high level language like C# would be a niche concept, but still a fun idea to implement.  I've made plenty of esolangs in my day, but they've all been;
+
+- hard to use
+- completely broken
+- high level
+
+To try and shake this esolang up, I decided to create something that *looks* like it's low level, but actually is high level.
+
+## What do you mean by that?
+
+Instead of directly communicating with the computer, this language is interpreted in C#. This not only makes it easier to implement (C# isn't as hard as base assembly), but it's also compatible with anything that supports C# without having to create a million different variations of it to support different hardware.
+
+## How do I get started?
+
+First, make sure your computer can execute C# code. Download .NET Core from Microsoft's website, then open a command prompt and navigate to a folder of your choosing. Then, run;
+
+`git clone https://github.com/TeleKiwi/KASM/wiki.git`
+
+Still in the command prompt, navigate to src/main. Then, run;
+
+`csc Program.cs`
+
+An executable should (hopefully) be generated. Run it. Then, when you see the prompt (`> `), paste the directory of your KASM file. (It must end in .kasm, or you'll get an error. Also, don't surround the path with quotation marks.) It should get interpreted!
---
+++ src/main/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace main
+{
+    class Program
+    {
+        public static void Main() {
+            Console.Title = "KASM Interpreter";
+
+            while(true) {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                Compiler.Compile(input);
+            }
+            
+        }
+    }
+
+    enum SyntaxTypes
+    {
+        and = 0x00,
+        or = 0x01,
+        nor = 0x02
+    }
+
+    class Compiler
+    {
+        static string[] code;
+        static List<string> stack = new List<string>();
+        
+        
+        public static string stringBetweenChars(string input, char charFrom, char charTo) {
+        int posFrom = input.IndexOf(charFrom);
+        if (posFrom != -1) { // if found char
+        
+            int posTo = input.IndexOf(charTo, posFrom + 1);
+            if (posTo != -1) { // if found char
+            
+                return input.Substring(posFrom + 1, posTo - posFrom - 1);
+            }
+        }
+        return string.Empty;
+        }
+
+        public static bool IsTextFileEmpty(string fileName) {
+        var info = new FileInfo(fileName);
+        if (info.Length == 0)
+            return true;
+        return false;
+        }
+
+        public static void Compile(string input) {
+            Locate(input);
+        }
+
+        static void Locate(string input) {
+            try {
+                if(IsTextFileEmpty(input)) {
+                    Error.throwWarning(1);
+                }
+                code = File.ReadAllLines(input);
+                if(Path.GetExtension(input) != ".kasm") {
+                    Error.throwError(2);
+                }
+            } catch(System.Exception)  {
+                Error.throwError(1);
+                return;
+            }
+        }
+    }
+
+
+    class Error
+    {
+        public static void throwError(int errorCode) {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("FATAL ERROR: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            switch(errorCode) {
+                case 1:
+                    Console.WriteLine("file does not exist.");
+                    break;
+                case 2:
+                    Console.WriteLine("invalid file extension. file extensions must be '.kasm'.");
+                    break;
+                case 3:
+                    Console.WriteLine("an argument is empty.");
+                    break;
+                case 4:
+                    Console.WriteLine("no 'main' procedure.");
+                    break;
+                case 5:
+                    Console.WriteLine("unknown keyword.");
+                    break;
+                case 6:
+                    Console.WriteLine("procedure is missing 'end' keyword.");
+                    break;
+
+            }
+            Console.WriteLine($"process halted with error code {errorCode}.");
+            Program.Main();
+
+        }
+
+        public static void throwWarning(int warningCode) {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("WARNING: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            switch(warningCode) {
+                case 1:
+                    Console.WriteLine("the file linked is empty. double check you have the right file?");
+                    break;
+
+            }
+            Console.WriteLine($"program will continue execution. warning code {warningCode}.");
+        }
+
+        public static void throwInfo(int infoCode) {
+            Program.Main();
+        }
+    }
+}
